aac7c82418b147c9b473bb2c8a1dfbcd4324bdf1
bquang2k6 <phambbquang30@gmail.com>
2025-09-29 18:08:06 +0700

    links

--- api/links.js
+++ api/links.js
@@ -2,29 +2,12 @@ import dbConnect from "../lib/mongodb.js";
 import Link from "../models/Link.js";
 import jwt from "jsonwebtoken";
 import cheerio from "cheerio";
-import Cors from "cors";
-
-const cors = Cors({
-  origin: ["https://fe-locket-tdtu.vercel.app", "http://localhost:5173", "https://locket-tdtu.wangtech.top"],
-  methods: ["GET", "POST", "OPTIONS"],
-  credentials: true,
-});
-
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) return reject(result);
-      return resolve(result);
-    });
-  });
-}
 
 const PASSWORD_RE = /^[A-Za-z0-9](?:25|24|23|22|21)[A-Za-z0-9]\d{4}$/;
 
 export default async function handler(req, res) {
   await dbConnect();
-  // chạy cors trước
-  await runMiddleware(req, res, cors);
+  await applyCors(req, res);  // 👉 luôn chạy CORS trước
 
   if (req.method === "POST") {
     // Check JWT
